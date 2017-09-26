ccc0e0e7b4329de1ce6c0b553f8fae29d7e97c92
Andri <Andri@DESKTOP-00QT1MO>
2017-09-26 13:39:52 +0300

    #2

---
+++ src/App.java
@@ -0,0 +1,42 @@
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.Arrays;
+
+public class App {
+
+	public static void main(String[] args) {
+		App appObject = new App();
+
+//		System.out.println("sayHello() result: " + appObject.sayHello("David") + "\n");
+
+//		System.out.println("generateHtmlElement() result: " + appObject.generateHtmlElement("h4", "Väiksem pealkiri"));
+	
+
+	}
+
+	// Ülesanne: tervita kasutajat, tervitusteksti saad ise valida.
+	// Kasutajale nime saad parameetrina.
+
+	private String sayHello(String name) {
+		return "Hello " + name;
+	}
+
+	// generateHtmlElement(“h1”, “This is a Heading”) -> “<h1>This is a
+	// Heading</h1>”
+	private String generateHtmlElement(String tag, String value) {
+		// 1. variant
+		// return "<" + tag + ">" + value + "</" + tag + ">";
+
+		// 2. variant
+		// String htmlElement = String.format("<%1$s>%2$s</%1$s>", tag, value);
+
+		// 3.variant
+		StringBuilder htmlElement = new StringBuilder("<");
+
+		return htmlElement.append(tag).append(">").append(value).append("</").append(tag).append(">").toString();
+
+	}
+	
+	//Etteantud nädala(või muu periood) temperatuurid, avutada keskmine temperatuur sel nädalal.
+	
+
+}
