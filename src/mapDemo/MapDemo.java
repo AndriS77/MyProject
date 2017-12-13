b991e99d9a15dfd80e39c7c449a324f17f7a5d24
andri sellik <andri.sellik@google.com>
2017-12-13 11:42:51 +0200

    13.12

--- src/mapDemo/MapDemo.java
+++ src/mapDemo/MapDemo.java
@@ -14,13 +14,14 @@ public class MapDemo {
 	//	System.out.println(people.get(2));
 		for (Integer key : people.keySet()) {
 			System.out.println(key + " " + people.get(key));
-		}*/
+		}*/ 
+		
 	// Uue mapi loomine 
 		MapDemo md = new MapDemo();
 		
 		Map<Integer, String> demoMap = new HashMap<Integer, String>();
 		
-		md.myMethod(demoMap);
+		md.myMethod(demoMap); 
 		
 		// Mapi uue vıti-v‰‰rtuspaari lisamine.
 		demoMap.put(1, "Valdur");
@@ -35,11 +36,26 @@ public class MapDemo {
 		
 		//Paari emaldamine
 		demoMap.remove(2);
-		System.out.println(demoMap);
-		
+		System.out.println(demoMap); 
 		//Kontroll, kas vastava vıtmega paar on olemas
-		System.out.println(demoMap.containsKey(1));
+		System.out.println(demoMap.containsKey(1 ));
+		System.out.println("______________________");
+		//Mapi l‰bimine
+		for (Map.Entry<Integer, String> entry : demoMap.entrySet()) {
+		System.out.println("Vˆti: " + entry.getKey() + " v‰‰rtus" + entry.getValue());
 	}
+		System.out.println("______________________");
+		//Mapi kıik vıtmed
+		System.out.println(demoMap.keySet());
+		for (Integer  key : demoMap.keySet()) {
+			System.out.println(key);
+		}
+		System.out.println("______________________");
+		//Mapi kıik v‰‰rtused
+		System.out.println("Mapi kıik v‰‰rtused: " + demoMap.values());
+		}
+	 
+	
 	
 	public String myMethod(Map<Integer, String> map ) {
 		return map.get(1);
---
+++ src/year_2/Kodutoo2.java
@@ -0,0 +1,27 @@
+package year_2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Kodutoo2 {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, String> persons = new HashMap<Integer, String>();
+		persons.put(2, "Indrek");
+		persons.put(17, "Kalmer");
+		persons.put(3, "Mati");
+		
+		}
+	
+		private int minKey(Set nums) {
+			
+			for (Object object : nums) {
+				return (int) object;
+			}
+			return 0;
+		}
+	}
+
+
