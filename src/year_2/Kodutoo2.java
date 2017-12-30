2a1f7fbe25d7df59275a458b28235379fc1b5e67
andris77 <andrisellik@gmail.com>
2017-12-30 15:14:26 +0200

    30.12

--- src/year_2/Kodutoo2.java
+++ src/year_2/Kodutoo2.java
@@ -1,6 +1,9 @@
 package year_2;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -14,18 +17,21 @@ public class Kodutoo2 {
 		persons.put(17, "Kalmer");
 		persons.put(3, "Mati");
 		
+		lowestKeyValue(persons,7);
       //  Entry<Integer, String> entry = persons.ceilingEntry();
 		//System.out.println(persons.firstKey());
 		
 		}
 	
-		private int minKey(Set nums) {
-			
-			for (Object object : nums) {
-				return (int) object;
+		public static Map<Integer,String> lowestKeyValue(Map<Integer,String> map, int key){
+			List<Integer> keys = new ArrayList<>();
+			map.forEach((k,v)->{
+				keys.add(k);});
+			int lowest = Collections.min(keys);
+			map.put(key,map.get(lowest));
+			map.remove(lowest);
+			return map;
 			}
-			return 0;
-		}*/
-	}
-
+		}
+	
 
