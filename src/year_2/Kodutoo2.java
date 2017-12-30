c235e5e6d0afc97b3f7fb504c15abc6e904d7c5b
andris77 <andrisellik@gmail.com>
2017-12-30 15:00:35 +0200

    30.12

--- src/year_2/Kodutoo2.java
+++ src/year_2/Kodutoo2.java
@@ -2,16 +2,20 @@ package year_2;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 public class Kodutoo2 {
 
 	public static void main(String[] args) {
 		
-		Map<Integer, String> persons = new HashMap<Integer, String>();
+		HashMap<Integer, String> persons = new HashMap<>();
 		persons.put(2, "Indrek");
 		persons.put(17, "Kalmer");
 		persons.put(3, "Mati");
+		
+      //  Entry<Integer, String> entry = persons.ceilingEntry();
+		//System.out.println(persons.firstKey());
 		
 		}
 	
@@ -21,7 +25,7 @@ public class Kodutoo2 {
 				return (int) object;
 			}
 			return 0;
-		}
+		}*/
 	}
 
 
