bd79c61cc5761c3a720ad16320fa345b0eb4d52f
andri sellik <andri.sellik@google.com>
2018-02-07 11:49:51 +0200

    :D 07.02

---
+++ src/Headphone/App.java
@@ -0,0 +1,18 @@
+package Headphone;
+
+public class App {
+
+	public static void main(String[] args) {
+		Headphone korvaKlapid1 = new Headphone("Bludio", 50, false);
+		System.out.println(korvaKlapid1);
+		System.out.println("Kas on kallis: " + korvaKlapid1.isExpensiveString());
+		System.out.println(korvaKlapid1.isExpensive() ? "Kõrvaklapid on kallid" : "Kõrvaklapid on odavad");
+		
+		Headphone korvaKlapid2 = new Headphone("Samsung");
+		korvaKlapid2.setPrice(154.50);
+		System.out.println(korvaKlapid2);
+		
+		Student student1 = new Student();
+		System.out.println(student1);
+	}
+}
