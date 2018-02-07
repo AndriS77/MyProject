57efe34abb5265625399e07c200e3871aa3ef29e
andri sellik <andri.sellik@google.com>
2018-02-07 09:18:39 +0200

    lol :D

--- src/UT/KT_4_1.java
+++ src/UT/KT_4_1.java
@@ -1,22 +1,21 @@
 package UT;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class KT_4_1 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		int b, c;
 		System.out.printf("Sisestage algarv:");
 		int P = scanner.nextInt();
-		for (int i = 2; i <= P; i++) {
-			if (P % i == 0 || i == 1) {
-				System.out.printf("Arv %d. pole algarv ning jagub arvudega: " +"1"+ i, P);
+		List<Integer>nums = new ArrayList<>();
+		for (int i = 2; i < P; i++) {
+			if (P % i == 0) {
+			//	nums.add(e);
 			}
 		}
-		/*
-		 * int i = 2; while (i <= (P - 1)) { if (P % i == 0) {
-		 * System.out.printf("Arv %d. pole algarv ning jagub arvudega: "+i,P); break; }
-		 * else { System.out.printf("Algarv"); } i++; }
-		 */
 	}
 }
