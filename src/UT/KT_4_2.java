afca9043481763e76869bf3c4b563d2b261a14a4
andris77 <andrisellik@gmail.com>
2018-02-09 15:57:31 +0200

    09.02

--- src/UT/KT_4_2.java
+++ src/UT/KT_4_2.java
@@ -8,34 +8,32 @@ public class KT_4_2 {
 
 	public static void main(String[] args) {
 		System.out.printf("Alustame mängu.\n");
-		
-//		double N = (-1000 + Math.random() * 1000);
+
+		// double N = (-1000 + Math.random() * 1000);
 
 		int N = ThreadLocalRandom.current().nextInt(-1000, 1000 + 1);
 		System.out.println(N);
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.printf("Sisestage uus arv:");
-		int X = scanner.nextInt();
-		
-		int counter = 11;
-		
-		while(X > N || X < N && counter > 0) {
-		if (X < N) {
-			System.out.printf("Peidetud arv on suurem.");
-			counter--;
-		} else if (X > N) {
-			System.out.println("Peidetud arv on väiksem.");
-			counter--;
-		} else  {
-			System.out.printf("Peidetud arv on %f", N);
-			counter = counter - counter;
-		}/*else {
-			System.out.printf("Te kaotasite. Peidetud arv oli %d",N);
-		}
-		
-		*/
-	}
 
-}
+		int counter = 11;
+
+		while (counter > 0) {
+			System.out.printf("Sisestage uus arv:\n");
+			int X = scanner.nextInt();
+
+			if (X < N) {
+				System.out.printf("Peidetud arv on suurem.\n");
+				counter--;
+			} else if (X > N) {
+				System.out.println("Peidetud arv on väiksem.\n");
+				counter--;
+			} else {
+				System.out.printf("Peidetud arv on %d", N);
+				break;
+			} 		}
+		if (counter == 0) {
+			System.out.printf("Te kaotasite. Peidetud arv oli %d.",N);
+		}
+
+	}
 }
