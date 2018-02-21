de837bc37c149fca9796bbb491b8a540658a1c45
andris77 <andrisellik@gmail.com>
2018-02-21 08:44:44 +0200

    21.02

--- src/Headphone/Kodutcar.java
+++ src/Headphone/Kodutcar.java
@@ -3,8 +3,13 @@ package Headphone;
 public class Kodutcar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Car auto1 = new Car("GT", "sport", "EV 001", "24.09.2007");
+		System.out.println(auto1);
+		
+		auto1.addOwner("Tiit Sukk");
+		auto1.addOwner("Eve Libe");
+		
+		System.out.println(auto1);
 	}
 
 }
